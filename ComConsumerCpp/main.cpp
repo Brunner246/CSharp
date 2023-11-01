f8f3d91ee2d5f055923cec007a03126a140e46fe
Michael Brunner <brunner@cadwork.swiss>
2023-11-01 08:53:00 +0100

    first commit

--- ComConsumerCpp/main.cpp
+++ ComConsumerCpp/main.cpp
@@ -6,17 +6,14 @@
 int main() {
   CoInitialize(nullptr);
 
-  // Create an instance of the C# COM object
   CSharp::IHelloWorldInterfacePtr pHelloWorld(
       __uuidof(CSharp::HelloWorldClass));
 
-  // Access the method in the C# COM object
   BSTR bstrGreeting;
   pHelloWorld->GetGreeting(&bstrGreeting);
 
   wprintf(L"%s\n", bstrGreeting);
 
-  // Free the BSTR
   SysFreeString(bstrGreeting);
 
   CoUninitialize();
